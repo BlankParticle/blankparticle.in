8d429d612ae0b910d8ff51e5d8bcaf97a559201a
BlankParticle <blankparticle@gmail.com>
2024-06-30 22:25:31 +0530

    fix: github redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   redirects: async () =>
     [
-      ["/(github/gh)", "https://github.com/BlankParticle"],
+      ["/(github|gh)", "https://github.com/BlankParticle"],
       ["/anime", "https://myanimelist.net/animelist/BlankParticle"],
       ["/music", "https://open.spotify.com/user/31krf3flzpa44udfgkc5a5xrqn7y"],
       ["/discord", "https://discord.gg/GRz7F2BRmp"],
