77ae1f238e4eafafee2bcd9bc36810427c5c598d
BlankParticle <blankparticle@gmail.com>
2024-07-14 23:11:24 +0530

    chore: add a WIP page to avoid 404

---
+++ src/app/[...wip]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-full flex-1 items-center justify-center">
+      <p className="text-2xl font-bold">This is a work in progress page.</p>
+    </div>
+  );
+}
